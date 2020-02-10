bdc5cc67cfa2bc75e1843589ef8e7711e906cf25
Jonatan <jgarcia_alt@outlook.com>
2020-02-10 10:42:32 -0600

    Implementando CRUD

--- services/products.js
+++ services/products.js
@@ -6,28 +6,43 @@ class ProductsService {
         this.collection = "products";
         this.mongoDB = new MongoLib();
     }
-
+    
     async getProducts({ tags }) {
-        const query = tags && {tags: { $in: tags}};
+        const query = tags && { tags: { $in: tags } };
         const products = await this.mongoDB.getAll(this.collection, query);
+    
         return products || [];
     }
-
-    getProduct({ productId }) {
-        return Promise.resolve(productsMocks[0]);
+    
+    async getProduct({ productId }) {
+        const product = await this.mongoDB.get(this.collection, productId);
+        return product || {};
+    }
+    
+    async createProduct({ product }) {
+        const createProductId = await this.mongoDB.create(this.collection, product);
+    
+        return createProductId;
+    }
+    
+    async updateProduct({ productId, product }) {
+        const updateProductId = await this.mongoDB.update(
+        this.collection,
+        productId,
+        product
+        );
+    
+        return updateProductId;
     }
 
-    createProduct({ product }) {
-        return Promise.resolve(productsMocks[0]);
+    async deleteProduct({ productId }) {
+        const deletedProductId = await this.mongoDB.delete(
+        this.collection,
+        productId
+        );
+    
+        return deletedProductId;
     }
-
-    updateProduct({ productId, product }) {
-        return Promise.resolve(productsMocks[0]);
     }
-
-    deleteProduct({ productId }) {
-        return Promise.resolve(productsMocks[0]);
-    }
-}
-
-module.exports = ProductsService;
+    
+    module.exports = ProductsService;
